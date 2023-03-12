d39e424018adb3188875f1c2826ea84bbd03c176
Inna7777 <118475702+Inna7777@users.noreply.github.com>
2023-03-12 21:39:17 +0300

    Apply suggestions from code review
    Co-authored-by: Danil <49741194+samodurOFF@users.noreply.github.com>

--- HW_seven/Task_7_2.py
+++ HW_seven/Task_7_2.py
@@ -9,6 +9,6 @@ def print_operation_table(operation, num_rows = 6, num_columns = 6): #функц
         row = [] # задаем пустую строку
         for y in range(1, num_columns+1): 
             row.append(str(operation(x, y))) # записываем елемент функции lambda в строку
-        print((row)) 
+        print(row) 
     
 print_operation_table(lambda x, y: x * y) # функция вычислений значений елементов таблицы
